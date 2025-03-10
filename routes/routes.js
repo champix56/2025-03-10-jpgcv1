a8c2bd8afbf80606ec252f25f307720f3b2f6fb7
champix56 <desorbaix@free.fr>
2025-03-10 18:41:43 +0100

    creation d'un objet de configuration de route

---
+++ routes/routes.js
@@ -0,0 +1,38 @@
+var routes = [
+  {
+    title: `editor`,
+    templateUrl: "/routes/editor/editor.html",
+    onLoad: function (params) {
+        console.log('editor chargé',params)
+    },
+    path: /^\/editor(\/((?<id>)\d*))?$/,
+  },
+  {
+    title: "thumbnail",
+    templateUrl: "/routes/thumbnail/thumbnail.html",
+    path: "/thumbnail",
+  },
+  {
+    title: "Credits",
+    template:
+      "<h1>remerciements :</h1><ul><li>Formateur : Alexandre DESORBAIX</li><li>Tous les participants de la formation</li></ul>",
+    path: "/credits",
+  },
+  {
+    title: "home",
+    templateUrl: "/routes/home/home.html",
+    path: "/",
+    default: true,
+  },
+];
+var errorsRoutes = {
+  404: {
+    title: "404 NOT FOUND",
+    templateUrl: "/routes/errors/404.html",
+  },
+  500: {
+    title: "500 Internal server error",
+    template:
+      "<h1>La page ne peut etre rendu</h1><h2>Erreur 500 : internal server error</h2>",
+  },
+};
