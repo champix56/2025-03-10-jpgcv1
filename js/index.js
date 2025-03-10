a568d58f772936370d27f5740369de1320414cce
champix56 <desorbaix@free.fr>
2025-03-10 18:09:56 +0100

    gestion de routes avec chargement par url et navigation in browser par navigate

--- js/index.js
+++ js/index.js
@@ -13,15 +13,22 @@ function routeAnalyze() {
   var wrapper = document.getElementById("wrapper");
   switch (path) {
     case "/editor":
+      wrapper.innerHTML = "<h1>Bienvenue sur l'éditeur de meme'</h1>";
       break;
     case "/thumbnail":
       wrapper.innerHTML =
         "<h1>Bienvenue sur la gallery des memes enregistrés</h1>";
-        break;
+      break;
     case "/":
     default:
       wrapper.innerHTML =
         "<h1>Bienvenue sur le site de generation de memes en svg avec js</h1>";
       break;
   }
+}
+function navigate(route) {
+  if (undefined === route || route.length === 0) route = "/";
+  if (route[0] !== "/") route = "/" + route;
+  history.pushState(undefined, undefined, route);
+  routeAnalyze();
 }
