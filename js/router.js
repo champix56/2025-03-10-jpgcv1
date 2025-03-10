7d9cb4912680649d8810e4fccfc2b350ecb18abb
champix56 <desorbaix@free.fr>
2025-03-10 19:01:42 +0100

    filtrage et chargement de la vue dans le wrapper

--- js/router.js
+++ js/router.js
@@ -1,24 +1,31 @@
-function Router() {
+function Router(wrapperId) {
   function routeAnalyze() {
     var path = location.pathname;
     console.log(path);
     var wrapper = document.getElementById("wrapper");
-    var m=undefined;
-    var currentRoute=routes.find(function (route) {
-        if(route.path instanceof RegExp && (m = route.path.exec(path)) !== null){
-            return true;
-        }
-        else if(route.path instanceof String && route.path===path) {
-            return true;
-        }
-        return false;
+    var m = undefined;
+    var currentRoute = routes.find(function (route) {
+      if (
+        route.path instanceof RegExp &&
+        (m = route.path.exec(path)) !== null
+      ) {
+        return true;
+      } else if (typeof route.path === "string" && route.path === path) {
+        return true;
+      }
+      return false;
     });
-    currentRoute.params=m.groups;
-    if(currentRoute!==null)loadRoute(currentRoute);
-    else loadRoute(errorsRoutes[404])
+    if (m !== null) currentRoute.params = m.groups;
+    if (currentRoute !== undefined) loadRoute(currentRoute);
+    else loadRoute(errorsRoutes[404]);
   }
-  function loadRoute(route){
+  function loadRoute(route) {
     console.log(route);
+    var wrapper = document.querySelector("#" + wrapperId);
+    wrapper.innerHTML = route.template;
+    if (undefined !== route.onLoad && route.onLoad instanceof Function) {
+      route.onLoad(route.params);
+    }
   }
   function navigate(route) {
     if (undefined === route || route.length === 0) route = "/";
@@ -31,4 +38,4 @@ function Router() {
   this.routeAnalyze = routeAnalyze;
   this.navigate = navigate;
 }
-var router = new Router();
+var router = new Router("wrapper");
