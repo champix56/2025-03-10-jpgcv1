f76e52da64863054f4f8c5bc8a5bc663bd33a63a
champix56 <desorbaix@free.fr>
2025-03-11 09:57:56 +0100

    creation d'instance d'objet par function es5

--- js/index.js
+++ js/index.js
@@ -4,31 +4,5 @@ document.addEventListener("DOMContentLoaded", function (evt) {
   //   notLoadedTag.innerHTML = "JS <br/>Loaded";
   //   notLoadedTag.style.backgroundColor = "green";
   notLoadedTag.remove();
-  routeAnalyze();
+  router.routeAnalyze();
 });
-
-function routeAnalyze() {
-  var path = location.pathname;
-  console.log(path);
-  var wrapper = document.getElementById("wrapper");
-  switch (path) {
-    case "/editor":
-      wrapper.innerHTML = "<h1>Bienvenue sur l'éditeur de meme'</h1>";
-      break;
-    case "/thumbnail":
-      wrapper.innerHTML =
-        "<h1>Bienvenue sur la gallery des memes enregistrés</h1>";
-      break;
-    case "/":
-    default:
-      wrapper.innerHTML =
-        "<h1>Bienvenue sur le site de generation de memes en svg avec js</h1>";
-      break;
-  }
-}
-function navigate(route) {
-  if (undefined === route || route.length === 0) route = "/";
-  if (route[0] !== "/") route = "/" + route;
-  history.pushState(undefined, undefined, route);
-  routeAnalyze();
-}
---
+++ js/router.js
@@ -0,0 +1,35 @@
+
+
+function Router() {
+  var _currentPath="/";
+  var _privateFieldOnlyInInstance="coucou";
+  function _routeAnalyze() {
+    var path = location.pathname;
+    console.log(path);
+    var wrapper = document.getElementById("wrapper");
+    switch (path) {
+      case "/editor":
+        wrapper.innerHTML = "<h1>Bienvenue sur l'éditeur de meme'</h1>";
+        break;
+      case "/thumbnail":
+        wrapper.innerHTML ="<h1>Bienvenue sur la gallery des memes enregistrés</h1>";
+        break;
+      case "/":
+      default:
+        wrapper.innerHTML ="<h1>Bienvenue sur le site de generation de memes en svg avec js</h1>";
+        break;
+    }
+  }
+  function _navigate(route) {
+    if (undefined === route || route.length === 0) route = "/";
+    if (route[0] !== "/") route = "/" + route;
+    history.pushState(undefined, undefined, route);
+    this.currentPath=route;
+    _routeAnalyze();
+  }
+
+  this.navigate=_navigate;
+  this.routeAnalyze=_routeAnalyze;
+  this.currentPath=_currentPath;
+}
+var router=new Router();
