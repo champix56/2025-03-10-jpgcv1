d0ca9da67d460d2d4ef4da6c978990f3bc3f1b7b
champix56 <desorbaix@free.fr>
2025-03-11 20:16:32 +0100

    format class de Routeur

--- js/router.js
+++ js/router.js
@@ -1,38 +1,56 @@
 /**
- * createur d'instance de routeur generique
+ * class de routeur generique
  * @param {[]} routes
  * @param {{}} errorRoutes
  */
-function Router(routes, errorRoutes) {
-  let _currentPath = "/";
-  const _privateFieldOnlyInInstance = "coucou";
-  const _routeAnalyze = () => {
-    const path = location.pathname;
+class Router {
+  #routes;
+  #errorRoutes;
+  #currentPath = "/";
+  #wrapper;
+  #wrapperId;
+  /**
+   * initialisation du router
+   * @param {Routes} routes 
+   * @param {ErrorRoutes} errorRoutes 
+   * @param {string} wrapperId 
+   */
+  constructor(routes, errorRoutes, wrapperId = "wrapper") {
+    this.#routes = routes;
+    this.#errorRoutes = routes;
+  }
+  /**
+   * analyze current url in url bar of browser
+   */
+  routeAnalyze() {
+    this.#currentPath = location.pathname;
     console.log(path);
-    const wrapper = document.getElementById("wrapper");
-    let currentRoute = routes.find((route) => {
-      if (typeof route.path === "string" && route.path === path) {
+    if (null === this.#wrapper) {
+      this.#wrapper = document.querySelector("#" + this.#wrapperId);
+    }
+    let currentRoute = this.#routes.find((route) => {
+      if (typeof route.path === "string" && route.path === this.#currentPath) {
         return true;
       } else if (route.path instanceof RegExp) {
-        const m = route.path.exec(path);
+        const m = route.path.exec(this.#currentPath);
         if (m === null) return false;
         else return true;
       } else false;
     });
 
     if (undefined === currentRoute) {
-      currentRoute = errorRoutes[404];
+      currentRoute = this.#errorRoutes[404];
     }
 
     if (currentRoute.template) {
-      _loadingTemplateInView(currentRoute);
+      this.#loadingTemplateInView(currentRoute);
     } else if (currentRoute.templateUrl) {
       const promiseFetch = fetch(currentRoute.templateUrl).then((r) =>
         r.text()
       );
       const timeOut = new Promise((resolved) => {
         setTimeout(() => {
-          resolved(errorRoutes[408]);
+          resolved(this.#errorRoutes[408]);
         }, 1000);
       });
       Promise.race([promiseFetch, timeOut]).then((resp) => {
@@ -40,46 +58,55 @@ function Router(routes, errorRoutes) {
           currentRoute = resp;
         } else {
           currentRoute.template = resp;
-          _loadingTemplateInView(currentRoute);
+          this.#loadingTemplateInView(currentRoute);
         }
       });
     } else {
-      _loadingTemplateInView(errorRoutes[500]);
+      this.#loadingTemplateInView(errorRoutes[500]);
     }
-  };
+  }
   /**
    * chargement de la route deja chargé(http) dans l'espace dedié a la vue(wrapper)
    * @param {Route} route
    */
-  const _loadingTemplateInView = (route) => {
-    wrapper.innerHTML = route.template;
-  };
+  #loadingTemplateInView(route) {
+    this.#wrapper.innerHTML = route.template;
+  }
 
   /**
    * navigation vers un path
-   * @param {string} route path a mettre en oeuvre
+   * @param {string} path path a mettre en oeuvre
    * @returns {undefined} ne retourne rien
    */
-  const _navigate = (route) => {
-    if (undefined === route || route.length === 0) route = "/";
-    if (route[0] !== "/") route = "/" + route;
-    history.pushState(undefined, undefined, route);
-    this.currentPath = route;
-    _routeAnalyze();
-  };
-  const _mapRouterLinks = (contextId) => {
+  navigate(path) {
+    if (undefined === path || path.length === 0) path = "/";
+    if (path[0] !== "/") path = "/" + path;
+    history.pushState(undefined, undefined, path);
+    this.#currentPath = path;
+    this.routeAnalyze();
+  }
+  mapRouterLinks(contextId) {
     document.querySelectorAll("#" + contextId + " a").forEach((element) => {
       element.addEventListener("click", (evt) => {
         evt.preventDefault();
         this.navigate(evt.target.href.replace(location.origin, ""));
       });
     });
-  };
+  }
 
-  this.mapRouterLinks = _mapRouterLinks;
-  this.navigate = _navigate;
-  this.routeAnalyze = _routeAnalyze;
-  this.currentPath = _currentPath;
+  /**
+   * get currentPath value of stored in router
+   */
+  get currentPath() {
+    return this.#currentPath;
+  }
+  /**
+   *set current path value and navigate
+   */
+  set currentPath(value) {
+    this.#currentPath = value;
+    this.navigate(value);
+  }
 }
 /**
  * instance globale du routeur
