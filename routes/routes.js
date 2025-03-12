0e303bce1e0c00e3f57edb809ecb114952fa1e74
champix56 <desorbaix@free.fr>
2025-03-12 22:06:54 +0100

    js native lazy loading

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
-import {Thumbnail} from './thumbnail/thumbnail.js'
-import {Editor} from './editor/editor.js'
+//import {Thumbnail} from  './thumbnail/thumbnail.js'
+//import { Editor } from "./editor/editor.js";
+const loadedModules = {};
 export const routes = [
   {
     path: "/",
@@ -9,12 +10,15 @@ export const routes = [
     path: "/thumbnail",
     templateUrl: "/routes/thumbnail/thumbnail.html",
     // template: "<h1>Bienvenue sur la gallery de meme</h1>",
-    onTemplateLoaded: (domNode) => {
-      console.log( domNode);
+    onTemplateLoaded: async (domNode) => {
+      console.log(domNode);
+      if (!loadedModules.thumbnailjs) {
+        loadedModules.thumbnailjs = await import("./thumbnail/thumbnail.js");
+      }
       if (!window.viewsjs) {
         window.viewsjs = {};
       }
-      window.viewsjs.Thumbnail = new Thumbnail();
+      window.viewsjs.Thumbnail = new loadedModules.thumbnailjs.Thumbnail();
       window.viewsjs.Thumbnail.initThumbnail(domNode);
     },
   },
@@ -22,12 +26,15 @@ export const routes = [
     path: /^\/editor(\/(?<id>\d*))?$/,
     // template: "<h1>Editor </h1>",
     templateUrl: "/routes/editor/editor.html",
-    onTemplateLoaded: (domNode, params) => {
+    onTemplateLoaded:async (domNode, params) => {
       console.log(params, domNode);
+      if (!loadedModules.editorjs) {
+        loadedModules.editorjs = await import("./editor/editor.js");
+      }
       if (!window.viewsjs) {
         window.viewsjs = {};
       }
-      window.viewsjs.Editor = new Editor();
+      window.viewsjs.Editor = new loadedModules.editorjs.Editor();
       window.viewsjs.Editor.initEditor(domNode, params);
     },
   },
